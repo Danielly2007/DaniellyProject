ffdce2c6c8000eaf7912cbdedfb4a04573a0f170
Danielly <daniellynerylove1@gmail.com>
2025-06-27 14:10:31 -0300

    Domain(sem getter and setter)

---
+++ project.danielly/src/main/java/com/recupera/project/danielly/domain/User.java
@@ -0,0 +1,14 @@
+package com.recupera.project.danielly.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+
+    private String name;
+    private String email;
+}
