a5c25ffb10e34ad618390a55973475a2179da076
Danielly <daniellynerylove1@gmail.com>
2025-06-27 13:57:41 -0300

    Controller

---
+++ project.danielly/src/main/java/com/recupera/project/danielly/controller/ProjectController.java
@@ -0,0 +1,29 @@
+package com.recupera.project.danielly.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello-world")
+public class ProjectController {
+	
+	@Autowired
+    private ProjectService projectService;
+
+	@GetMapping
+    public String project() {
+        return projectService.project(" Danielly");
+    }
+	
+	@PostMapping("/{id}")
+    public String projectPost(@PathVariable ("id") String id,@RequestParam(value = "filter", defaultValue = "nenhuma") String filter, @RequestBody User body) {
+        return "Hello World" + body.getName() + " " + body.getEmail() + id + filter;
+    }
+
+}
